25df1dc6d2cc22c2c4a99911405236eea12f270a
nilsnolde <nilsnolde@gmail.com>
2019-03-18 18:58:23 +0300

    add polyline5 and polyline6 decoding support

---
+++ routingpy/utils.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Modifications Copyright (C) 2019 GIS OPS UG
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+
+def _trans(value, index):
+    """
+    Copyright (c) 2014 Bruno M. Custódio
+    Copyright (c) 2016 Frederick Jansen
+
+    https://github.com/hicsail/polyline/commit/ddd12e85c53d394404952754e39c91f63a808656
+    """
+    byte, result, shift = None, 0, 0
+
+    while byte is None or byte >= 0x20:
+        byte = ord(value[index]) - 63
+        index += 1
+        result |= (byte & 0x1f) << shift
+        shift += 5
+        comp = result & 1
+
+    return ~(result >> 1) if comp else (result >> 1), index
+
+
+def _decode(expression, precision=5, is3d=False):
+    """
+    Copyright (c) 2014 Bruno M. Custódio
+    Copyright (c) 2016 Frederick Jansen
+
+    https://github.com/hicsail/polyline/commit/ddd12e85c53d394404952754e39c91f63a808656
+    """
+    coordinates, index, lat, lng, z, length, factor = [], 0, 0, 0, 0, len(expression), float(10 ** precision)
+
+    while index < length:
+        lat_change, index = _trans(expression, index)
+        lng_change, index = _trans(expression, index)
+        lat += lat_change
+        lng += lng_change
+        if not is3d:
+            coordinates.append((lat / factor, lng / factor))
+        else:
+            z_change, index = _trans(expression, index)
+            z += z_change
+            coordinates.append((lat / factor, lng / factor, z / 100))
+
+    return coordinates
+
+
+def decode_polyline5(polyline, is3d=False):
+    """Decodes an encoded polyline string which was encoded with a precision of 5.
+
+    :param polyline: An encoded polyline, only the geometry.
+    :type polyline: str
+
+    :param is3d: Specifies if geometry contains Z component. Currently only GraphHopper and OpenRouteService
+        support this. Default False.
+    :type is3d: bool
+
+    :returns: List of decoded coordinates with precision 5.
+    :rtype: list
+    """
+
+    return _decode(polyline, precision=5, is3d=is3d)
+
+
+def decode_polyline6(polyline, is3d=False):
+    """Decodes an encoded polyline string which was encoded with a precision of 6.
+
+    :param polyline: An encoded polyline, only the geometry.
+    :type polyline: str
+
+    :param is3d: Specifies if geometry contains Z component. Currently only GraphHopper and OpenRouteService
+        support this. Default False.
+    :type is3d: bool
+
+    :returns: List of decoded coordinates with precision 6.
+    :rtype: list
+    """
+
+    return _decode(polyline, precision=6, is3d=is3d)
---
+++ tests/test_utils.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Modifications Copyright (C) 2019 GIS OPS UG
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+"""Tests for utils module."""
+
+from routingpy import utils
+import tests as _test
+
+
+class UtilsTest(_test.TestCase):
+
+    def setUp(self):
+        self.coords2d_5prec = '__`t@smslHfo@~\\'
+        self.coords3d_5prec = '__`t@smslH_sVfo@~\\etjG'
+        self.coords2d_6prec = 'a_iqOaqkg}AxaN`kH'
+        self.coords3d_6prec = 'a_iqOaqkg}A_sVxaN`kHetjG'
+
+    def test_polyline5_2d_decoding(self):
+
+        decoded = [(8.68864, 49.42058), (8.68092, 49.41578)]
+        self.assertEqual(decoded, utils.decode_polyline5(self.coords2d_5prec))
+
+    def test_polyline5_3d_decoding(self):
+
+        decoded = [(8.68864, 49.42058, 120.96), (8.68092, 49.41578, 1491.39)]
+        self.assertEqual(decoded, utils.decode_polyline5(self.coords3d_5prec, True))
+
+    def test_polyline6_2d_decoding(self):
+
+        decoded = [(8.688641, 49.420577), (8.680916, 49.415776)]
+        self.assertEqual(decoded, utils.decode_polyline6(self.coords2d_6prec))
+
+    def test_polyline6_3d_decoding(self):
+
+        decoded = [(8.688641, 49.420577, 120.96), (8.680916, 49.415776, 1491.39)]
+        self.assertEqual(decoded, utils.decode_polyline6(self.coords3d_6prec, True))
