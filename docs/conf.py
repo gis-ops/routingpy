904a40c4c37ecebff5c99c50ebcd799dcac88318
nilsnolde <nils.nolde@gmail.com>
2022-11-12 20:36:34 +0100

    update deps; test 3.11; add christian to authors; add sphinx extension for strike-through text

--- docs/conf.py
+++ docs/conf.py
@@ -30,11 +30,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.intersphinx", "sphinxnotes.strike"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
